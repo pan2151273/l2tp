0af0cd31d5c6d3695e9f573ed65f287029069ee2
panyuan <panyuanyuan@tongji.edu.cn>
2024-03-26 17:50:36 +0800

    shell

--- shell.sh
+++ shell.sh
@@ -5,8 +5,8 @@ server_ip=$(curl -s http://whatismyip.akamai.com/)
 
 # 设置用户名、密码以及 IPSec 预共享密钥
 username="2151273"
-password="TONGJIjiayou,.09"
-ipsec_pre_shared_key="TONGJIjiayou,.09"
+password="2151273"
+ipsec_pre_shared_key="2151273"
 
 # 输出阿里云服务器的公网 IP 地址
 echo "阿里云服务器 IP 地址为：$server_ip"
